e17fe50d307a7eaecb503a9a43a67c5194a27a50
cairodasilvapinto <131769429+cairodasilvapinto@users.noreply.github.com>
2023-08-18 23:48:11 -0300

    mandando sprint 3 em produção

---
+++ SPRINT3/fundamentos/ExerciciosParte1.py
@@ -0,0 +1,45 @@
+# E1
+import datetime
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+ano = datetime.datetime.now().year
+print(nome, ano + (100 - idade))
+
+
+# E2
+def verifica_par(n):
+    if n % 2 == 0:
+        return "Par:"
+    else:
+        return "Ímpar:"
+
+
+for i in range(3):
+    n = int(input())
+    print(verifica_par(n), n)
+
+# E3
+for i in range(0, 21, 2):
+    print(i)
+
+
+# E4
+def verifica_primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+for i in range(1, 101):
+    if verifica_primo(i):
+        print(i)
+
+# E5
+dia = 22
+mes = 10
+ano = 2022
+print(f"{dia}/{mes}/{ano}")
