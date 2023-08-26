ae31f3bdd927002df124cb6c6c3e7db26693c589
cairodasilvapinto <131769429+cairodasilvapinto@users.noreply.github.com>
2023-08-26 17:16:55 -0300

    Mando curos até a parte de poo avançada

---
+++ SPRINT3/poo_avancada/contador_objetos.py
@@ -0,0 +1,15 @@
+class ClasseSimples:
+    contador = 0
+
+    def __init__(self):
+        # self.__class__.contador += 1
+        self.contar()
+
+    @classmethod
+    def contar(cls):
+        cls.contador += 1
+
+
+if __name__ == "__main__":
+    lista = [ClasseSimples(), ClasseSimples(), ClasseSimples()]
+    print(ClasseSimples.contador)  # Esperado 3
