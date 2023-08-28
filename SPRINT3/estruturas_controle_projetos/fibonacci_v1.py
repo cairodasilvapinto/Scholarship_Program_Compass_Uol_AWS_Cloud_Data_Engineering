368cabc2d4475460176eb7cf9b1530343fd4a694
cairodasilvapinto <131769429+cairodasilvapinto@users.noreply.github.com>
2023-08-28 14:14:51 -0300

    Mandando pasta SPRINT4 com progamação funcamental

---
+++ SPRINT3/estruturas_controle_projetos/fibonacci_v1.py
@@ -0,0 +1,14 @@
+# 0, 1, 1, 2, 3, 5, 8, 13, 21...
+def fibonacci():
+    penultimo = 0
+    ultimo = 1
+    print(f"{penultimo},{ultimo}", end=",")
+    while True:
+        proximo = penultimo + ultimo
+        print(proximo, end=",")
+        penultimo = ultimo
+        ultimo = proximo
+
+
+if __name__ == "__main__":
+    fibonacci()
