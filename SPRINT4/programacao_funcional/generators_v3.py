d6f8c508fd0f9e11dfbf88d642cf865e801a8230
cairodasilvapinto <131769429+cairodasilvapinto@users.noreply.github.com>
2023-08-28 19:20:15 -0300

    mandando arquivo de programação fundamental

---
+++ SPRINT4/programacao_funcional/generators_v3.py
@@ -0,0 +1,19 @@
+def sequence():
+    num = 0
+    while True:
+        num += 1
+        yield num
+
+
+if __name__ == "__main__":
+    seq = sequence()
+
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
+    print(next(seq))
