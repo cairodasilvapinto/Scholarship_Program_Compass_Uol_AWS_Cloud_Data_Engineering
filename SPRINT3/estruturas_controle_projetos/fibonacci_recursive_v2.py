cb06c23453e14d703727add51842725b6fbb416f
cairodasilvapinto <131769429+cairodasilvapinto@users.noreply.github.com>
2023-08-28 14:14:51 -0300

    Mandando pasta SPRINT4 com progamação funcamental

---
+++ SPRINT3/estruturas_controle_projetos/fibonacci_recursive_v2.py
@@ -0,0 +1,13 @@
+def fibonacci(quantidade, sequencia=(0, 1)):
+    # Importante: Condição de parada
+    return (
+        sequencia
+        if len(sequencia) == quantidade
+        else fibonacci(quantidade, sequencia + (sum(sequencia[-2:]),))
+    )
+
+
+if __name__ == "__main__":
+    # Listar os 20 primeiros números da sequência
+    for fib in fibonacci(20):
+        print(fib)
---
+++ SPRINT4/programacao_funcional/filter.py
@@ -0,0 +1,14 @@
+pessoas = [
+    {"nome": "Pedro", "idade": 11},
+    {"nome": "Mariana", "idade": 18},
+    {"nome": "Arthur", "idade": 26},
+    {"nome": "Rebeca", "idade": 6},
+    {"nome": "Tiago", "idade": 19},
+    {"nome": "Gabriela", "idade": 17},
+]
+
+menores = filter(lambda p: p["idade"] < 18, pessoas)
+print(list(menores))
+
+nomes_grandes = filter(lambda p: len(p["nome"]) >= 7, pessoas)
+print(tuple(nomes_grandes))
