970f77cb23beff235eed5425907b4297ba02170a
cairodasilvapinto <131769429+cairodasilvapinto@users.noreply.github.com>
2023-08-28 19:20:15 -0300

    mandando arquivo de programação fundamental

---
+++ SPRINT4/programacao_funcional/funcoes_imutabilidade_v2.py
@@ -0,0 +1,15 @@
+from functools import reduce
+from operator import add
+
+valores = (30, 10, 25, 70, 100, 94)
+
+print(sorted(valores))
+print(valores)
+
+print(min(valores))
+print(max(valores))
+print(sum(valores))
+print(reduce(add, valores))
+print(reversed(valores))
+print(tuple(reversed(valores)))
+print(valores)
---
+++ SPRINT4/programacao_funcional/implementando_map_v2.py
@@ -0,0 +1,10 @@
+# Implementação simplificada do map
+
+
+def mapear(funcao, lista):
+    return (funcao(elemento) for elemento in lista)
+
+
+if __name__ == "__main__":
+    resultado = mapear(lambda x: x**2, [2, 3, 4])
+    print(tuple(resultado))
