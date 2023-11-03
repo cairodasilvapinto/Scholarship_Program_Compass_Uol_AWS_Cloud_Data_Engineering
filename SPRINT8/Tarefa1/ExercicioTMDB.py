56d3910925179f58e573a9a487006bb33ffec3c4
cairodasilvapinto <131769429+cairodasilvapinto@users.noreply.github.com>
2023-11-03 19:45:22 -0300

    mandando script do desafio para o repo

--- SPRINT8/Tarefa1/ExercicioTMDB.py
+++ SPRINT8/Tarefa1/ExercicioTMDB.py
@@ -25,3 +25,32 @@ for movie in data["results"]:
 
 df_final = pd.concat(filmes)
 display(df_final)
+
+Etapa 2 - Ingestão streaming/micro batch
+Neste etapa do desafio iremos capturar dados do TMDB via AWS Lambda realizando chamadas de API. Os dados coletados devem ser persistidos em Amazon S3, camada RAW Zone, mantendo o formato da origem (JSON) e, se possível, agrupando-os em arquivos com, no máximo, 100 registros cada.
+O objetivo desta etapa é complementar os dados dos Filmes e Series, carregados na Etapa 1, com dados oriundos  TMDB e, opcionalmente, de outra API de sua escolha.
+
+Importante:
+
+Os arquivos CSVs carregados na Etapa 1 não devem ser modificados.
+Os novos dados devem ser complementares aos dados do CSV. Tem que existir informações novas sobre os dados do CSV.
+Não é necessário realizar tratamento dos dados externos, o máximo que pode ser feito é o agrupamento de dados.
+Cuidado para os arquivos JSON gerados não serem maior do que 10 MB.
+Não agrupe JSON com estruturas diferentes.
+Os IDs do IMDB presentes nos arquivos CSV podem ser utilizados em pesquisas  no TMDB.
+Se você escolher fazer sobre um filme ou uma trilogia específica, considere utilizar pelo menos 4 métodos de API diferentes para possibilitar uma análise de dados qualificada.
+Considere desenvolver seu código localmente primeiro e com poucos dados para depois leva-lo para a AWS Lambda e aumentar a pesquisa de dados. APIs normalmente limitam requisições. Evite realizar muitas requisições em fase de desenvolvimento ou teste para evitarmos qualquer bloqueio na conta de vocês
+Esta atividade corresponde a parte do desafio final. Não esperamos que você registre resposta neste espaço. Contudo, deves adicionar o código-fonte produzido ao seu repositório no Github. Lembre-se de remover suas credenciais de acesso antes de efetuar commit.
+
+Perguntas dessa tarefa
+Em sua conta AWS, no serviço AWS Lambda, realize as seguintes atividades:
+
+1.  Criar nova camada (layer) no AWS Lambda para as libs necessárias à ingestão de dados (por exemplo,  tweepy, se você utilizar o Tweeter)
+
+2. Implementar o código Python em AWS Lambda para consumo de dados do TMDB:
+   - Se está utilizando TMDB,  buscar pela API os dados que complementem a análise
+   - Utilizar a lib boto3 para gravar os dados no AWS S3
+    -----| no momento da gravação dos dados deve-se considerar o padrão de path: <nome do bucket>\<camada de armazenamento>\<origem do dado>\<formato do dado>\<especificação do dado>\<data de processamento separada por ano\mes\dia>\<arquivo>
+              São exemplos de caminhos de arquivos válidos:
+               - S3:\\data-lake-do-fulano\Raw\Twitter\JSON\2022\05\02\prt-uty-nfd.json
+               - S3:\\data-lake-do-fulano\Raw\Twitter\JSON\2022\05\02\idf-uet-wqt.json
---
+++ SPRINT8/Tarefa2DesafioParte2/geradorJson.py
@@ -0,0 +1,76 @@
+import json
+import requests
+import boto3
+from datetime import date
+
+
+def lambda_handler(event, context):
+    # Crie o cliente S3 especificando a região
+    s3 = boto3.client("s3", region_name="us-east-1")
+
+    # Fazendo uma chamada API ao TMDB
+    url_discover = "https://api.themoviedb.org/3/discover/movie"
+    url_movie = "https://api.themoviedb.org/3/movie/"
+    querystring = {
+        "api_key": "a4b19d34fb649042fb1473575842df56",
+        "language": "pt-BR",
+        "sort_by": "vote_average.desc",
+        "include_adult": "false",
+        "include_video": "false",
+        "primary_release_date.gte": "2020-01-01",
+        "primary_release_date.lte": str(date.today()),
+        "vote_count.gte": "500",
+        "with_genres": "16,35",  # 16 para Animação e 35 para Comédia
+    }
+
+    all_movies = []
+    for page in range(1, 11):
+        querystring["page"] = str(page)
+        response_discover = requests.request("GET", url_discover, params=querystring)
+        data_discover = response_discover.json()
+
+        # Para cada filme, obtenha os detalhes do filme
+        for result in data_discover["results"]:
+            querystring["append_to_response"] = "credits"
+            response_movie = requests.request(
+                "GET", url_movie + str(result["id"]), params=querystring
+            )
+            data_movie = response_movie.json()
+            movie_details = {
+                "id": data_movie["id"],
+                "title": data_movie["title"],
+                "revenue": data_movie["revenue"],
+                "budget": data_movie["budget"],
+                "vote_average": data_movie["vote_average"],
+                "vote_count": data_movie["vote_count"],
+                "main_actor": next(
+                    (
+                        cast["name"]
+                        for cast in data_movie["credits"]["cast"]
+                        if cast["order"] == 0
+                    ),
+                    None,
+                ),
+                "director": next(
+                    (
+                        crew["name"]
+                        for crew in data_movie["credits"]["crew"]
+                        if crew["job"] == "Director"
+                    ),
+                    None,
+                ),
+            }
+            all_movies.append(movie_details)
+
+        # Dividindo os dados em grupos de 100 registros
+        chunks = [all_movies[i : i + 100] for i in range(0, len(all_movies), 100)]
+
+        for i, chunk in enumerate(chunks):
+            # Gravando os dados no S3
+            s3.put_object(
+                Body=json.dumps(chunk),
+                Bucket="desafioetl1",
+                Key=f"desafioetl1/Raw/tmdb/json/2023/11/12{page}_{i}.json",
+            )
+
+    return {"statusCode": 200, "body": json.dumps("Dados gravados com sucesso no S3!")}
